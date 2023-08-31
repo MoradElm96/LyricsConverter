0815ee1c166c5ec736df4a22cc4d54e04e80b824
Morad <Morad-96@hotmail.com>
2023-08-31 02:18:31 +0200

    Add project files.

---
+++ LyricsConverter/Vistas/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace LyricsConverter.Vistas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSeleccion = new System.Windows.Forms.Button();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(254, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "csv to lrc converter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seleccione un archivo CSV";
+            // 
+            // btnSeleccion
+            // 
+            this.btnSeleccion.Location = new System.Drawing.Point(229, 76);
+            this.btnSeleccion.Name = "btnSeleccion";
+            this.btnSeleccion.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccion.TabIndex = 2;
+            this.btnSeleccion.Text = "Select";
+            this.btnSeleccion.UseVisualStyleBackColor = true;
+            this.btnSeleccion.Click += new System.EventHandler(this.btnSeleccion_Click);
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(229, 132);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(75, 23);
+            this.btnConverter.TabIndex = 3;
+            this.btnConverter.Text = "Convert";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(321, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(467, 213);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.btnSeleccion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSeleccion;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ LyricsConverter/Vistas/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LyricsConverter.Vistas
+{
+    public partial class Form1 : Form
+    {
+
+        private DataTable dataTable;  // Declarar el DataTable a nivel de formulario
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataTable();  // Llama a este método en el constructor para crear la estructura de la tabla
+        }
+        private void InitializeDataTable()
+        {
+            dataTable = new DataTable();
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbrirCSV_Click(object sender, EventArgs e)
+        {
+        }
+        private void btnSeleccion_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos CSV (*.csv)|*.csv|Todos los archivos (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                // Leer el contenido del archivo CSV y cargarlo en el DataTable
+                dataTable.Clear();  // Limpiar los datos existentes en la tabla
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string[] columnNames = reader.ReadLine().Split(';');  // Leer y descartar la primera línea (encabezados)
+
+                    // Agregar columnas al DataTable si no existen
+                    foreach (string columnName in columnNames)
+                    {
+                        if (!dataTable.Columns.Contains(columnName))
+                        {
+                            dataTable.Columns.Add(columnName);
+                        }
+                    }
+
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        string[] values = line.Split(';');  // Usar punto y coma como separador
+
+                        // Agregar fila solo si la cantidad de valores coincide con la cantidad de columnas
+                        if (values.Length == dataTable.Columns.Count)
+                        {
+                            dataTable.Rows.Add(values);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            if (dataTable.Rows.Count == 0)
+            {
+                MessageBox.Show("No se ha cargado ningún archivo CSV.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Archivos LRC (*.lrc)|*.lrc|Todos los archivos (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string outputLrc = saveFileDialog.FileName;
+
+                GenerateLrcFile(outputLrc);
+
+                MessageBox.Show($"Archivo .lrc generado: {outputLrc}", "Conversión Completada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void GenerateLrcFile(string outputLrc)
+        {
+            using (StreamWriter lrcWriter = new StreamWriter(outputLrc, false, Encoding.UTF8))
+            {
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    if (row.ItemArray.Length >= 2) // Considera las columnas del archivo CSV (milisegundos, texto)
+                    {
+                        int milliseconds = Convert.ToInt32(row[0]);
+                        string text = row[1].ToString().Trim();
+
+                        string lrcTime = FormatLrcTime(milliseconds);
+                        string lrcLine = $"{lrcTime}{text}";
+
+                        lrcWriter.WriteLine(lrcLine);
+                    }
+                }
+            }
+        }
+
+        private string FormatLrcTime(int milliseconds)
+        {
+            int minutes = milliseconds / 60000;
+            int seconds = (milliseconds / 1000) % 60;
+            milliseconds %= 1000;
+            return $"[{minutes:D2}:{seconds:D2}.{milliseconds:D3}]";
+        }
+
+
+    }
+}
